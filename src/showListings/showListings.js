38f7df6578f53c397dfe78b7a7f9a83bf97dd7ff
seanHolahan <holahan.s@husky.neu.edu>
2019-04-12 18:46:50 -0700

    live events page and api connect working

--- src/showListings/showListings.js
+++ src/showListings/showListings.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 
-import './shows.css';
+import './showListings.css';
 import Fade from '@material-ui/core/Fade';
 import {
   CSSTransition,
@@ -9,16 +9,17 @@ import {
   Transition
 } from 'react-transition-group';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import ShowListings from '../showListings/showListings';
+import Moment from 'react-moment';
 
-class Shows extends Component {
+class ShowListings extends Component {
   constructor(props){
         super(props);
         this.state = {
 
           error: null,
           isLoaded: false,
-          tourDates:null
+          tourDates:null,
+
 
 
         }
@@ -36,6 +37,9 @@ class Shows extends Component {
                                           tourDates: response}))
       //  .then(response => console.log("heh",response.json()))
         .then(console.log("response",this.state))
+
+
+
   //       var bandsintown = require('bandsintown')('48fd7be4e116c62000215ecb1c4acc78');
   //       bandsintown
           // .getArtistEventList('Skrillex')
@@ -44,44 +48,53 @@ class Shows extends Component {
           // });
       }
 
-  render() {
-    console.log("response",this.state);
+
+      render() {
+        console.log("response",this.state);
+        const { error, isLoaded, tourDates } = this.state;
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
+          return (
+
+            <div>
+              {tourDates.map(show => (
+                <div className="showListing">
 
 
-    // <ul>
-    //   {tourDates.map(show => (
-    //     <li key={show.name}>
-    //       {show.name} {show.price}
-    //     </li>
-    //   ))}
-    // </ul>
+                    <Moment className="eventDate" format="D MMM YYYY">{show.datetime}</Moment>
+                    <h3>{show.venue.name}</h3>
+                    <h3>{show.venue.city},{show.venue.region}</h3>
 
 
+                    <a target="_blank" className="eventButton" href={show.url}>Buy Tickets</a>
 
-    // if (this.state.tourDates ===null) {
-    //
-    // } else {
-    //   const tourDates = this.state.tourDates.map((link) =>
-    //                <h2>hi</h2>
-    //   )
-    // }
+                </div>
 
-    return (
-
-
-
-          <div key={1} className="showContainer">
-          <h2>LIVE</h2>
-          <ShowListings></ShowListings>
-          </div>
+              ))}
+              </div>
 
 
 
+          );
+        }
 
 
 
-    );
-  }
-}
 
-export default Shows;
+
+        // if (this.state.tourDates ===null) {
+        //
+        // } else {
+        //   const tourDates = this.state.tourDates.map((link) =>
+        //                <h2>hi</h2>
+        //   )
+        // }
+
+
+      }
+    }
+
+    export default ShowListings;
