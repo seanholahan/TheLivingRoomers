1e69a1f72c3a9c522d062c81f6036e09c5b9c415
seanHolahan <holahan.s@husky.neu.edu>
2019-10-01 23:11:20 -0700

    Updates

---
+++ precache-manifest.d2e9c2f15a0de085df412a5c63fe9cd6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e73bdb8f5fa5af809e50d3d2c073cf8",
+    "url": "/TheLivingRoomers/index.html"
+  },
+  {
+    "revision": "07343fc0e889b3b70bb0",
+    "url": "/TheLivingRoomers/static/css/main.06359660.chunk.css"
+  },
+  {
+    "revision": "0c0d0f7e07c94f5962bc",
+    "url": "/TheLivingRoomers/static/js/2.3b025cf3.chunk.js"
+  },
+  {
+    "revision": "07343fc0e889b3b70bb0",
+    "url": "/TheLivingRoomers/static/js/main.58673cff.chunk.js"
+  },
+  {
+    "revision": "e6a893f530ec73be1b15",
+    "url": "/TheLivingRoomers/static/js/runtime-main.b688e4e4.js"
+  },
+  {
+    "revision": "7361d5962afb4f094b46dcb864d13260",
+    "url": "/TheLivingRoomers/static/media/Book.7361d596.JPG"
+  },
+  {
+    "revision": "03837900865545f40f6e3070c1245ac2",
+    "url": "/TheLivingRoomers/static/media/Seaweed.03837900.ogg"
+  },
+  {
+    "revision": "be9281a36eb09e1e1bc64c6b3e6e6b43",
+    "url": "/TheLivingRoomers/static/media/bg.be9281a3.png"
+  },
+  {
+    "revision": "4c9473c1fdce5bd527dd36647d3aa3db",
+    "url": "/TheLivingRoomers/static/media/blue.4c9473c1.jpg"
+  },
+  {
+    "revision": "2430360efa2d43cf271d413cf4811024",
+    "url": "/TheLivingRoomers/static/media/bunny.2430360e.ogg"
+  },
+  {
+    "revision": "9a5711197f9572cfa7050dec2a319250",
+    "url": "/TheLivingRoomers/static/media/burnt.9a571119.jpg"
+  },
+  {
+    "revision": "49627943c5dd6124d2132d964000b2d1",
+    "url": "/TheLivingRoomers/static/media/cover.49627943.jpg"
+  },
+  {
+    "revision": "6b12243b8757637514e9125e4c8bad49",
+    "url": "/TheLivingRoomers/static/media/live.6b12243b.png"
+  },
+  {
+    "revision": "d68c615aeef4275ee9c8e73740d349ec",
+    "url": "/TheLivingRoomers/static/media/live.d68c615a.jpg"
+  },
+  {
+    "revision": "79d8b21df5cb45bac7d189b4a714f7f3",
+    "url": "/TheLivingRoomers/static/media/merch.79d8b21d.png"
+  },
+  {
+    "revision": "c711aeaeb16ddd661ec690e93219e22c",
+    "url": "/TheLivingRoomers/static/media/mobileHome.c711aeae.jpg"
+  },
+  {
+    "revision": "195386ab65967df8e96acf3711b70192",
+    "url": "/TheLivingRoomers/static/media/screwYou.195386ab.ogg"
+  },
+  {
+    "revision": "d91fc1abdcfe47ce178d3d722ccbc578",
+    "url": "/TheLivingRoomers/static/media/soundcloud.d91fc1ab.png"
+  },
+  {
+    "revision": "63d867018350a52de993fe35457d5800",
+    "url": "/TheLivingRoomers/static/media/spotify.63d86701.png"
+  }
+]);
